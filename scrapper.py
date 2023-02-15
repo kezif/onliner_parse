764bf138cd45f2a50451760cda1555d51e6d176a
Kirill <kezif6262@gmail.com>
2023-02-15 14:48:31 +0300

    wait till results load

--- scrapper.py
+++ scrapper.py
@@ -7,7 +7,7 @@ from selenium.webdriver.chrome.options import Options
 
 
 chrome_options = Options()  # get driver
-chrome_options.add_argument("--window-size=1820,980") # site is responsive. I want search bar right away
+#chrome_options.add_argument("--window-size=1820,980") # site is responsive.
 chrome_options.add_argument("--headless")
 driver = webdriver.Chrome(chrome_options=chrome_options)
 wait = WebDriverWait(driver, 10)
@@ -25,7 +25,7 @@ search.send_keys('ddr4')
 search_frame = wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="fast-search-modal"]/div/div/iframe'))) 
 driver.switch_to.frame(search_frame)  # switch to search result iframe
 
-
+wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="search-page"]/div[2]/ul/li[1]')))  # wait till results load
 search_results = driver.find_element(By.XPATH, '//*[@id="search-page"]/div[2]/ul')  # get results
 
 with open('to_parse.html', 'w', encoding='utf-8') as f:
